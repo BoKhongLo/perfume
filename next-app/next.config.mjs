8f03d66713c929a16fa44bbf8cb5c227c117e646
ducanh <ngocungducanh2022@gmail.com>
2026-04-01 21:07:02 +0700

    update cors

--- next-app/next.config.mjs
+++ next-app/next.config.mjs
@@ -1,49 +1,62 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: false,
-    env: {
-      NEXTAUTH_SECRET:"the-king-also-has-to-cum",
-    },
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'localhost',
-                pathname: '**',
-            },
-            {
-                protocol: 'http',
-                hostname: 'localhost',
-                pathname: '**',
-            },
-            {
-                protocol: 'http',
-                hostname: 'upload.wikimedia.org',
-                pathname: '**',
-            },
-            {
-                protocol: 'https',
-                hostname: 'upload.wikimedia.org',
-                pathname: '**',
-            },
+  reactStrictMode: false,
+  env: {
+    NEXTAUTH_SECRET: "the-king-also-has-to-cum",
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "localhost",
+        pathname: "**",
+      },
+      {
+        protocol: "http",
+        hostname: "localhost",
+        pathname: "**",
+      },
+      {
+        protocol: "http",
+        hostname: "upload.wikimedia.org",
+        pathname: "**",
+      },
+      {
+        protocol: "http",
+        hostname: "160.25.81.4",
+        port: "3434",
+        pathname: "/media/uploads/**",
+      },
+      {
+        protocol: "https",
+        hostname: "upload.wikimedia.org",
+        pathname: "**",
+      },
+    ],
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,DELETE,PATCH,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
         ],
-    },
-    eslint: {
-        ignoreDuringBuilds: true,
-    },
-    async headers() {
-        return [
-            {
-                source: "/api/:path*",
-                headers: [
-                    { key: "Access-Control-Allow-Credentials", value: "true" },
-                    { key: "Access-Control-Allow-Origin", value: "*" }, 
-                    { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
-                    { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-                ]
-            }
-        ]
-    }
+      },
+    ];
+  },
 };
 
 export default nextConfig;
